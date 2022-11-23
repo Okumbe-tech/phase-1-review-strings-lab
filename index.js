a834033e499d42662f0fc97ed8d58fc474445bd5
Okumbe-tech <evanceokumbe97@gmail.com>
2022-11-23 16:36:13 +0300

    learning review-string-lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+const myString = 'template literal';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
